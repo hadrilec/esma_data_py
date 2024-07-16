233d9f18507244abfe477caac60b5127f2739097
Hadrien Leclerc <hadrien.leclerc@esma.europa.eu>
2024-07-16 10:42:29 +0200

    second commit

--- esma_data_py/__init__.py
+++ esma_data_py/__init__.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 11 15:36:13 2024
-
-@author: gdegl'innocenti
-"""
 
 from esma_data_py.download_register_data import get_mifid_file_list, download_file, get_fca_firds_file_list, get_ssr_exempted_shares, get_last_full_files
 
@@ -11,7 +6,3 @@ __all__ = ["get_mifid_file_list", "download_file", "get_fca_firds_file_list",
            "get_ssr_exempted_shares", "get_last_full_files"
            ]
 
-
-# import os
-# os.chdir("M:/%5C/temp/gdeglinnocenti/Package/")
-# os.chdir('M:\\temp\\gdeglinnocenti\\Package')
