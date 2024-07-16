4b5c17c29523bb38dd329ecace01d1d43613f849
Giorgio Degl'Innocenti <giorgio.deglinnocenti@esma.europa.eu>
2024-07-16 17:42:22 +0200

    Structured package

--- esma_data_py/__init__.py
+++ esma_data_py/__init__.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from esma_data_py.download_register_data import get_mifid_file_list, download_file, get_fca_firds_file_list, get_ssr_exempted_shares, get_last_full_files
-
-__all__ = ["get_mifid_file_list", "download_file", "get_fca_firds_file_list",
-           "get_ssr_exempted_shares", "get_last_full_files"
-           ]
-
+from .mifid import *
+from .ssr import *
