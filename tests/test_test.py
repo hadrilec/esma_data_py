c4b4a921ac7270f6bb6b1b3e4fb2786cfe0b9d4d
Giorgio Degl'Innocenti <giorgio.deglinnocenti@esma.europa.eu>
2024-08-27 15:10:33 +0200

    additional unit tests

--- tests/test_test.py
+++ tests/test_test.py
@@ -13,10 +13,11 @@ import functools
 
 os.chdir(r"M:\%5C\temp\gdeglinnocenti\pkg\esma_data_py")
 
-from esma_data_py.ssr.get_ssr_exempted_shares import get_ssr_exempted_shares
-from esma_data_py.mifid.get_mifid_file_list import get_mifid_file_list
-from esma_data_py.mifid.download_file import download_file
-
+from esma_data_py.ssr.get_ssr_exempted_shares import get_ssr_exempted_shares # Theoretically complete
+from esma_data_py.mifid.get_mifid_file_list import get_mifid_file_list # Theoretically complete
+from esma_data_py.mifid.download_file import download_file, download_one_file, _download_one_file # To check
+from esma_data_py.mifid.get_fca_firds_file_list import get_fca_firds_file_list # Theoreticaly complete
+from esma_data_py.mifid.get_last_full_files import get_last_full_files
 
 class MyTests(unittest.TestCase):
 
@@ -26,16 +27,46 @@ class MyTests(unittest.TestCase):
     
         def test_get_last_full_files(self):
             test  = get_ssr_exempted_shares()
-            self.assertTrue(isinstance(test, pd.DataFrame)
-        
-        def test_get_mifid_file_list(self):
-            test = get_mifid_file_list('firds')
             self.assertTrue(isinstance(test, pd.DataFrame))
+        
+        # THIS SHOULD BE RUNNED BY get_last_full_files
+        # def test_get_mifid_file_list_1(self):
+        #         test = get_mifid_file_list('firds')
+        #         self.assertTrue(isinstance(test, pd.DataFrame))
+
+        def test_get_mifid_file_list_2(self):    
+                test = get_mifid_file_list('dvcap')
+                self.assertTrue(isinstance(test, pd.DataFrame))  
     
         def test_download_file(self):
-            test = download_file("http://fitrs.esma.europa.eu/fitrs/FULNCR_20240622_D_2of6.zip")
+            test = download_file("http://fitrs.esma.europa.eu/fitrs/FULNCR_20240622_D_2of6.zip", update = True)
             self.assertTrue(isinstance(test, pd.DataFrame))
+        
+        def test_download_one_file(self):
+                test = download_one_file("http://fitrs.esma.europa.eu/fitrs/FULNCR_20240622_D_2of6.zip", update=True)
+                self.assertTrue(isinstance(test, pd.DataFrame))
+        
+        def test_get_fca_firds_file_list_1(self):
+                test = get_fca_firds_file_list('firds')
+                self.assertTrue(isinstance(test, pd.DataFrame))
             
+        def test_get_fca_firds_file_list_2(self):
+                test = get_fca_firds_file_list('dvcap')
+                self.assertTrue(isinstance(test, pd.DataFrame))
+            
+        def test_get_last_full_files_1(self):
+                test = get_last_full_files(eqt = True, cfi = 'C')
+                self.assertTrue(isinstance(test, pd.DataFrame))
+            
+        def test_get_last_full_files_2(self):
+                test = get_last_full_files(eqt = False, cfi = None)
+                self.assertTrue(isinstance(test, pd.DataFrame)) 
+                
+        def test_get_last_full_files_3(self):
+                self.assertRaises(ValueError, get_last_full_files, cfi='X')
+
+
+          
                         
 if __name__ == '__main__':
     unittest.main()
