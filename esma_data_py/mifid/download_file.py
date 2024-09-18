f8054fccd2b41280d6b1a880ae4e4074eeadc036
Giorgiodgl <105796522+Giorgiodgl@users.noreply.github.com>
2024-09-18 14:12:45 +0200

    Update download_file.py

--- esma_data_py/mifid/download_file.py
+++ esma_data_py/mifid/download_file.py
@@ -119,16 +119,16 @@ def download_file(url_list, update=False, multiprocess=True, cpu=10):
     Downloads data from a list of URLs and consolidates the results into a single DataFrame.
 
     Args:
-    - url_list (list or str): A single URL or a list of URLs from which data is to be downloaded. If a single URL is provided, it is converted into a list.
+       url_list (list or str): A single URL or a list of URLs from which data is to be downloaded. If a single URL is provided, it is converted into a list.
     
-    - update (bool): If True, allows for the updating of files during the download process. Defaults to False.
+       update (bool): If True, allows for the updating of files during the download process. Defaults to False.
     
-    - multiprocess (bool): Controls whether to use multiprocessing to parallelize the download process.
+       multiprocess (bool): Controls whether to use multiprocessing to parallelize the download process.
     
-    - cpu (int): Specifies the maximum number of CPU cores to use for multiprocessing. Defaults to 10.
+       cpu (int): Specifies the maximum number of CPU cores to use for multiprocessing. Defaults to 10.
 
     Returns:
-    - pd.DataFrame: A DataFrame containing all the data fetched from the provided URLs.
+       pd.DataFrame: A DataFrame containing all the data fetched from the provided URLs.
 
     Examples:
        >>> # Example of downloading data from multiple URLs without multiprocessing:
