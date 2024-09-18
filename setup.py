7bf318f17d30edcad6ef577f11652f48a22f3f63
Hadrien Leclerc <66560093+hadrilec@users.noreply.github.com>
2024-09-18 11:10:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
     
 setuptools.setup(
-    name = "data_py",
+    name = "esma_data_py",
     version="0.0.1",
     author = "Hadrien Leclerc",
     author_email="hadrien.leclerc@esma.europa.eu",
